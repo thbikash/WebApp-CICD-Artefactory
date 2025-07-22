1decbebb183ab454cf66ee990dbc71e80623ffcf
Bikash Thapa <bikashthazure2025@gmail.com>
2025-07-22 11:38:41 -0400

    Server.js changed to test CI

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure DevOps CI/CD!');
+  res.send('Hello from Azure DevOps CI/CD! Learning');
 });
 
 app.listen(port, () => {
