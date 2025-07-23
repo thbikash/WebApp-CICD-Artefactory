6bad08ada322676663cb7e26c99d25f68fba0ea6
Bikash Thapa <bikashthazure2025@gmail.com>
2025-07-23 16:02:31 -0400

    Updated version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure DevOps CI/CD! Learning');
+  res.send('Hello from Azure DevOps CI/CD! Learning Version 1.0');
 });
 
 app.listen(port, '0.0.0.0', () => {
