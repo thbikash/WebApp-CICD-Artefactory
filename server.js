45c0704f5af645088d28f4211d686efca1d6080e
Bikash Thapa <bikashthazure2025@gmail.com>
2025-07-28 16:30:59 -0400

    version upgraded

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure DevOps CI/CD! Artefactory deployment');
+  res.send('Hello from Azure DevOps CI/CD! Artefactory deployment V1.0.0');
 });
 
 app.listen(port, '0.0.0.0', () => {
